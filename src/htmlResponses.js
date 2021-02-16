48695fb230658934c04ce808fa78d1786e206499
jksands <jksands83@gmail.com>
2021-02-16 17:40:30 -0500

    added modules to random-joke-plus

---
+++ src/htmlResponses.js
@@ -0,0 +1,21 @@
+// 5 - here's our 404 page
+const errorPage = `
+<html>
+    <head>
+        <title>404 - File Not Found!!</title>
+    </head>
+    <body>
+        <h1>404 - File Not Found!</h1>
+        <p>Check your URL, or your typing!!</p>
+        <p>Diphthong</p>
+    </body>
+</html>
+`;
+
+const get404Response = (request, response) => {
+    response.writeHead(404, { 'Content-Type': 'text/html' }); // send response headers
+    response.write(errorPage); // send content
+    response.end(); // close connection
+}
+
+module.exports.get404Response = get404Response;
